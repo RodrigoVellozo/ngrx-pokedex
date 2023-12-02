e704d458cd5a83ef2e5a6947e402cf3ab8a1ae07
Rodrigo Vellozo <rodrigovellozosi@gmail.com>
2023-12-02 15:21:02 -0300

    adicionar angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedComponent } from './ui/components/shared/shared.component';
+import { NavbarComponent } from './ui/components/navbar/navbar.component';
+import { CardComponent } from './ui/components/card/card.component';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SharedComponent,
+    NavbarComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,6 +29,10 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     StoreModule.forRoot(),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({}),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
